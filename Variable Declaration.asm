bcdb4eadd69dbd00c87415dcf48f40db67531334
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-03-17 15:18:06 +0600

    Create Variable Declaration.asm

---
+++ Variable Declaration.asm
@@ -0,0 +1,27 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+VAR1 DB 10
+
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    
+    MOV AH,2
+    ADD VAR1,48
+    MOV DL,VAR1
+    INT 21H
+    
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
+    
+    
+
+
