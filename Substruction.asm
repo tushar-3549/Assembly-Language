8da34cb36de375bceabb49687649e159e1433e91
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-03-17 15:23:43 +0600

    Create Substruction.asm

---
+++ Substruction.asm
@@ -0,0 +1,44 @@
+.model small
+.stack 100h
+.data
+a db 'Inter first digit : $'
+b db 10,13,'Inter second digit : $'
+c db 10,13,'Result : $'
+.code
+
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a
+    int 21h
+    
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    mov ah,9
+    lea dx,b
+    int 21h
+    
+    mov ah,1
+    int 21h
+    mov bh,al 
+    
+    mov ah,9
+    lea dx,c
+    int 21h 
+    
+    sub bl,bh   ; bl = bl - bh
+    add bl,48
+    mov ah,2
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
